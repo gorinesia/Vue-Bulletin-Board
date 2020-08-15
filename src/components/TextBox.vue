57abf0e61e831db229f3f086f858bf7b4e4bf600
Akinori Sakakibara <gorion.1213@gmail.com>
2020-08-15 23:57:54 +0900

    initial commit for bulletin board

---
+++ src/components/TextBox.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="textbox-container">
+    <textarea placeholder="ここに入力" class="textbox-input" v-model.trim="body"></textarea>
+    <div class="textbox-button">
+      <Button title="投稿" :onClick="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from './Button';
+
+export default {
+  components: {
+    Button
+  },
+  props: {
+    onPost: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      body: ''
+    }
+  },
+  methods: {
+    post() {
+      if (!this.body) {
+        alert('何か入力して下さい');
+        return;
+      }
+
+      const newMessage = this.createMessage();
+      this.onPost(newMessage);
+      this.body = '';
+    },
+    createMessage() {
+      return {
+        date: new Date().toLocaleString(),
+        body: this.body
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.textbox-container {
+  padding: 10px;
+}
+
+.textbox-input {
+  width: 100%;
+  height: 100px;
+  resize: none;
+  background: white;
+  border: 1px solid black;
+  border-radius: 5px; 
+}
+
+.textbox-button {
+  margin: 10px;
+  text-align: right;
+}
+</style>
